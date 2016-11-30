b0c348515f38772000379af5f5cb838522a6d7db
raccy <open@raccy.org>
2016-11-30 22:55:58 +0900

    requireすると良いみたい

---
+++ src/js/index.ts
@@ -0,0 +1,13 @@
+import * as jQuery from "jquery";
+const $ = jQuery;
+(<any>window).jQuery = (<any>window).$ = jQuery;
+import * as Tether from "tether";
+(<any>window).Tether = Tether;
+require("bootstrap");
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Main from "../lib/component/main";
+
+const mainContent = document.getElementById("main");
+ReactDOM.render(React.createElement(Main, null), mainContent);
