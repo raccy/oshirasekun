d407d976b72d7942885ed829d72ce282905c1011
raccy <open@raccy.org>
2016-12-02 06:20:10 +0900

    requireはできるようになった

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ class MainApplication {
     mainWindow: Electron.BrowserWindow = null;
 
     constructor(public app: Electron.App) {
-        this.app.on("window-all-closed", this.onWindowAllClosed);
+        this.app.on("window-all-closed", () => this.onWindowAllClosed());
         this.app.on("ready", this.onReady);
     }
 
     onWindowAllClosed() {
-        if (process.platform !== "darwin") {
-            this.app.quit();
-        }
+        this.app.quit();
     }
 
     onReady() {
@@ -20,8 +18,8 @@ class MainApplication {
             height: 400,
             minWidth: 500,
             minHeight: 200,
-            acceptFirstMouse: true,
-            titleBarStyle: "hidden"
+            acceptFirstMouse: true
+            // titleBarStyle: "hidden"
         });
 
         this.mainWindow.loadURL("file://" + __dirname + "/index.html");
