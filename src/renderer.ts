248ea34f0f82e2db25195d9ae78c5da417324e41
raccy <open@raccy.org>
2016-12-25 14:30:26 +0900

    ちょっとレンダラーにも書いてみた

--- src/renderer.ts
+++ src/renderer.ts
@@ -9,5 +9,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Main from "./lib/component/main";
 
+import { createStore, applyMiddleware, compose } from "redux";
+const ReduxElectronStore = require("redux-electron-store"); // no typess
+const electronEnhancer = ReduxElectronStore.electronEnhancer;
+import reducer from "./lib/reducers";
+// import { setDebugMode } from "./lib/actions";
+
+// Redux Store
+const filter = {
+    mode: true
+};
+const enhancer = electronEnhancer({
+    filter,
+    dispachProxy: a => store.dispatch(a)
+});
+const store = createStore(reducer, {}, enhancer);
+
 const mainContent = document.getElementById("main");
 ReactDOM.render(React.createElement(Main, null), mainContent);
